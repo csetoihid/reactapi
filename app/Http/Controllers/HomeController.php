b2724c6fe0c93549cf77671998c7233ed94f40a7
csetoihid <cse.toihid@gmail.com>
2020-11-15 23:07:07 +0100

    updates

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\HomeETCModel;
+use App\HomeEtcModel;
 class HomeController extends Controller
 {
     function onSelectVideoVideo(){
