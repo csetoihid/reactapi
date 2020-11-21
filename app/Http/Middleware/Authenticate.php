0bdbf3a208f0dd1e1ada5665a2e605fe422763a5
csetoihid <cse.toihid@gmail.com>
2020-11-21 03:15:21 +0100

    ssss

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -37,6 +37,6 @@ class Authenticate
     {
         return $next($request)
         ->header('Access-Control-Allow-Origin','*')
-        ->header('Access-Control-Allow-Methods','*');
+        ->header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
     }
 }
